90dd97f6e8dfeb261af55d2783168e0a8e730978
Dan Jaheny <danjaheny@mac.home>
2025-02-03 16:41:11 +0100

    add pubkey

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ from fastapi import FastAPI, Request
 app = FastAPI()
 
 load_dotenv()
-FORDEFI_PUBLIC_KEY = os.getenv("FORDEFI_PUBLIC_KEY")
+public_key_path = os.getenv("FORDEFI_PUBLIC_KEY_PATH")
+with open(public_key_path, "r") as f:
+    FORDEFI_PUBLIC_KEY = f.read()
 signature_pub_key = ecdsa.VerifyingKey.from_pem(FORDEFI_PUBLIC_KEY)
 
 async def verify_sig(request: Request):
@@ -52,3 +54,5 @@ async def fordefi_webhook(request: Request):
         print(status_message)
 
     return {"message": "Webhook received"}
+
+# uvicorn app:app --host 0.0.0.0 --port 8000
